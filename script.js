afe2f05322251c465a1015187c01358171d10205
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-10-14 21:47:22 +0530

    Finished algorithm
    Updated & finished the Js algorithm using Splash API.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const accessKey = "ajMvFapgHpFebkWDMoT2j543fZ6WBDyNfn5y1E-9Bes";
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const showMoreBtn = document.getElementById("show-more-btn");
+const showMoreBtn = document.getElementById("show-more-button");
 
 
 let keyword = "";
@@ -11,16 +11,37 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        image.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+    page++;
     searchImages();
 })
